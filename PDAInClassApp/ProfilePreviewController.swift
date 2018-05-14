d2996021d0d31b67135dd1d3e7fbb5517602a9a4
Joe Nelson <joeandrewnelson@gmail.com>
2018-05-14 02:03:33 -0700

    Initial Commit after a delete of Github in the project and a remake.
    Also Week 6 homework complete,  REDO as Github error erased all of my app delegate and all of my PreviewController.Swift files

--- PDAInClassApp/ProfilePreviewController.swift
+++ PDAInClassApp/ProfilePreviewController.swift
@@ -9,9 +9,44 @@
 import UIKit
 
 final class ProfilePreviewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var aboutText: UITextView!
+    @IBOutlet weak var ageText: UITextField!
+    
+    //: MARK Profile Properties
+    var profileData: Profile?
+    
+    // MARK: Initialization.
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "mikey"
+        // Set data for the view.
+        guard let profileData = profileData else {return}
+        setProfileData(profileData: profileData)
+        viewDidLayoutSubviews()
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        // Make Imageview a Circle
+        profileImage.layer.cornerRadius = profileImage.frame.width/2
+    }
+    
+    func setProfileData (profileData: Profile) {
+        
+        // Set the title to the name of the username from the profile and the other
+        // Field Data from the profile.swift file
+        
+        self.title = profileData.username
+        
+        self.aboutText.text = profileData.about
+        
+        self.ageText.text = profileData.age
+        
+        
+        guard let imageUrl = profileData.url, let url = URL(string: imageUrl) else {return}
+            self.profileImage.kf.setImage(with: url)
+        }
 }
